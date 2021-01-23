367f03a2ca86495f6384cfa829996d2549d734e1
Juniorjainsahab <p.jain3322@gmail.com>
2021-01-23 23:12:13 +0530

    formating date accordingly

--- app/src/main/java/com/squarebaot/newsapp/activity/ArticleBrowseActivity.java
+++ app/src/main/java/com/squarebaot/newsapp/activity/ArticleBrowseActivity.java
@@ -14,6 +14,10 @@ import com.squarebaot.newsapp.model.Article;
 import com.squarebaot.newsapp.network.Constant;
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -24,6 +28,8 @@ import static com.squarebaot.newsapp.R.id.description;
 import static com.squarebaot.newsapp.R.id.read_full_story;
 import static com.squarebaot.newsapp.R.id.source;
 import static com.squarebaot.newsapp.R.id.title;
+import static com.squarebaot.newsapp.network.Constant.DESIRED_STRING_FORMAT;
+import static com.squarebaot.newsapp.network.Constant.ORIGINAL_STRING_FORMAT;
 
 public class ArticleBrowseActivity extends AppCompatActivity {
 
@@ -58,9 +64,29 @@ public class ArticleBrowseActivity extends AppCompatActivity {
         Picasso.get().load(article.getUrlToImage()).fit().centerCrop().into(articleImageView);
         titleTextView.setText(article.getTitle());
         sourceTextView.setText(article.getSource().getName());
-        createdAtTextView.setText(article.getPublishedAt());
+        createdAtTextView.setText(formatDate(article.getPublishedAt()));
         descriptionTextView.setText(article.getContent() == null ? article.getDescription()
                 : article.getContent().split("[+]")[0]);
+    }
+
+    private String formatDate(String publishedAt) {
+        SimpleDateFormat readingFormat = new SimpleDateFormat(ORIGINAL_STRING_FORMAT);
+        SimpleDateFormat outputFormat = new SimpleDateFormat(DESIRED_STRING_FORMAT);
+        Date date = null;
+        try {
+            date = readingFormat.parse(publishedAt);
+            System.out.println(outputFormat.format(date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String[] formattedDate = date.toString().split(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder.append(formattedDate[2])
+                .append("-")
+                .append(formattedDate[1])
+                .append("-")
+                .append(formattedDate[5])
+                .append(" at ").append(formattedDate[3]).toString();
     }
 
     @OnClick(read_full_story)
--- app/src/main/java/com/squarebaot/newsapp/network/Constant.java
+++ app/src/main/java/com/squarebaot/newsapp/network/Constant.java
@@ -4,4 +4,6 @@ public class Constant {
     public static final String BASE_URL = "https://newsapi.org";
     public static final String API_KEY = "7cb8b3ac7c4745b5a8ba6f50becde20d";
     public static final String ARTICLE = "article";
+    public static final String ORIGINAL_STRING_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    public static final String DESIRED_STRING_FORMAT = "dd-MMM-yyyy'T'HH:mm:ss'Z'";
 }
